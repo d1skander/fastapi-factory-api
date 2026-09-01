ed0381b37bb67c28dcdee792b0c67b8eba17a052
d1skander <maksimcydypov8@gmail.com>
2026-09-01 22:04:14 +0800

    add_order_model_new_value

--- models/main_models.py
+++ models/main_models.py
@@ -1,21 +1,30 @@
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import UUID, Column, String, Numeric, CheckConstraint
+from sqlalchemy import UUID, Column, String, Numeric, CheckConstraint, ForeignKey
 from sqlalchemy.dialects.postgresql import ENUM
 
 from decimal import Decimal
 
-from shemas.main_shemas import BoilerStatus
+from shemas.main_shemas import BoilerStatus, FactoryRank
 
 
 import uuid
 
 
-class Base(DeclarativeBase):
-    pass
+class Base(DeclarativeBase): pass
 
 
-class Order():
+class Order(Base):
+    __tablename__ = "orders"
     id = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    status: Mapped[ENUM] = mapped_column(ENUM(BoilerStatus, create_type=False), nullable=False, default=BoilerStatus.IN_QUEUE)
+    status: Mapped[ENUM] = mapped_column(ENUM(BoilerStatus, create_type=False), nullable=False, default=BoilerStatus.IN_QUEUE) #create_type как понимаю это просто чтобы не создавать снова тип в базе данных
     boiler: Mapped[str] = mapped_column(String(30), nullable=False, default='no info')
     power: Mapped[Decimal] = mapped_column(Numeric(scale=2, precision=6), CheckConstraint('power >= 0'), nullable=False, default=0.0)
+    who_work = mapped_column(UUID(as_uuid=True), ForeignKey("workers.id"))
+
+
+class Worker(Base):
+    __tablename__ = "workers"
+    id = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    surname: Mapped[str] = mapped_column(String(50), nullable=False)
+    grades: Mapped[str] = mapped_column(ENUM(FactoryRank, create_type=False), nullable=False, default=FactoryRank.RANK_1_2)
